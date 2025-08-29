df760b07cdaf7f71b734cda092c87a9af047630d
澶山稵杞 <163642689+pisces-thamsan@users.noreply.github.com>
2025-08-29 15:10:26 +0800

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,13 +1,19 @@
 // 词典数据（改成let使其可以被重新赋值）
 let dictionaryData = [
+    // 名词
     {word: "Benfoure", phonetic: "/ˈbɛnfɔuɻ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "本弗尔；本弗尔王国", example: "Gericod Benfoure tias!<br>欢迎来到本弗尔王国！"},
-    {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "一，一个；一个的，单独的", example: "Moš necore cinisó tias.<br>我一个人来到城里面。"},
     {word: "Eirine", phonetic: "/ɛiˈɻʌinɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "我", example: "Eirine necore et. 我是人。"},
     {word: "Jogul", phonetic: "/ˈdʒɔkul/", partOfSpeech: "n.", grammaticalCase: "主宾同形", definition: "（本弗尔神话&珊教神话中的）使者；卿", example: "Benfou Montoré Jogul<br>梧桐十二卿"},
     {word: "zalone", phonetic: "/zʌˈlɔnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Màf zalone musbla scola daze musblitam dolént zalonseol aposisint zalon moš rimar.<br>每一粒沙都囚禁着一个世界，而世界本身，不过是另一片更辽阔沙海中的一粒沙。"},
     {word: "zalon", phonetic: "/zʌˈlɔn/", partOfSpeech: "n.", grammaticalCase: "宾格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Dal Musblae zalon et.<br>世界是一粒沙子。"},
     {word: "alcane", phonetic: "/ˈʌlkʰʌɪn/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "眼泪", example: "Kleš ragép ven gēnira hont alcane apont gēnira.<br>眉梢的汗珠啊，胜过眉下的眼泪。"},
     {word: "fulane", phonetic: "/fɯˈlʌnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "猫", example: "Kleš fulane Eirinam et.<br>这是我的猫。"},
+    // 动词
+    {word: "afuce", phonetic: "/ʌˈfjɯkʰɛ/", partOfSpeech: "v.", tense: "一般现在时", voice: "主动", definition: "加；增加", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
+    // 数词
+    {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.一；一个；adj.一个的；单独的", example: "Moš necore cinisó tias.<br>我一个人来到城里面。"},
+    {word: "nam", phonetic: "/nʌm/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.二；两；两个；adj.两个的；不专注的；有二心的", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
+    // 示例英文单词（24小时内删除）
     {
         word: "take",
         phonetic: "/teɪk/",
