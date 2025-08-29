107e7c0a9ebb94de0a45b7a4096d8c4329a95eae
澶山稵杞 <163642689+pisces-thamsan@users.noreply.github.com>
2025-08-29 14:29:01 +0800

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,17 +1,12 @@
 // 词典数据（改成let使其可以被重新赋值）
 let dictionaryData = [
     {word: "Benfoure", phonetic: "/ˈbɛnfɔuɻ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "本弗尔；本弗尔王国", example: "Gericod Benfoure tias! 欢迎来到本弗尔王国！"},
-    {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "一，一个；一个的，单独的", example: "Moš necore cinisó tias. 我一个人来到城里面。 "},
+    {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "一，一个；一个的，单独的", example: "Moš necore cinisó tias. 我一个人来到城里面。"},
     {word: "Eirine", phonetic: "/ɛiˈɻʌinɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "我", example: "Eirine necore et. 我是人。"},
     {word: "Jogul", phonetic: "/ˈdʒɔkul/", partOfSpeech: "n.", grammaticalCase: "主宾同形", definition: "（本弗尔神话&珊教神话中的）使者；卿", example: "Benfou Montoré Jogul 梧桐十二卿"},
-    {
-        word: "serendipity",
-        phonetic: "/ˌserənˈdɪpəti/",
-        partOfSpeech: "n.",
-        grammaticalCase: "主格",
-        definition: "意外发现珍奇事物的本领；不期而遇的好事",
-        example: "Finding this beautiful beach was pure serendipity."
-    },
+    {word: "zalone", phonetic: "/zʌˈlɔnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Maf zalone musbla scola daze musblitam<br>dolent zalonseol aposisint zalon mosh rimar.\n每一粒沙都囚禁着一个世界，而世界本身，不过是另一片更辽阔沙海中的一粒沙。"},
+    {word: "zalon", phonetic: "/zʌˈlɔn/", partOfSpeech: "n.", grammaticalCase: "宾格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Dal Musblae zalon et. 世界是一粒沙子。"},
+    {word: "alcane", phonetic: "/ˈʌlkʰʌɪn/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "眼泪", example: "Kleš ragép ven gēnira hont alcane apont gēnira. 眉梢的汗珠啊，胜过眉下的眼泪。"},
     {
         word: "take",
         phonetic: "/teɪk/",
@@ -94,25 +89,4 @@ let dictionaryData = [
         definition: "含糊的，不明确的；有歧义的",
         example: "His instructions were ambiguous and open to interpretation."
     },
-    {
-        word: "resilient",
-        phonetic: "/rɪˈzɪliənt/",
-        partOfSpeech: "adj.",
-        definition: "有弹性的；能迅速恢复的",
-        example: "Children are often more resilient than adults in adapting to new environments."
-    },
-    {
-        word: "verbose",
-        phonetic: "/vɜːˈbəʊs/",
-        partOfSpeech: "adj.",
-        definition: "冗长的，啰嗦的",
-        example: "His writing style is often verbose and could benefit from editing."
-    },
-    {
-        word: "diligent",
-        phonetic: "/ˈdɪlɪdʒənt/",
-        partOfSpeech: "adj.",
-        definition: "勤奋的，细致的",
-        example: "She is a diligent worker who always completes her tasks on time."
-    }
 ];
