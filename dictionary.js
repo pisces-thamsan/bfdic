5d005b489b35f360e69633e881b70d960d5d8bae
澶山稵杞 <163642689+pisces-thamsan@users.noreply.github.com>
2025-08-29 15:21:52 +0800

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,8 +1,10 @@
 // 词典数据（改成let使其可以被重新赋值）
 let dictionaryData = [
     // 名词
+    {word: "Alueté", phonetic: "/ˈʌlɯtʰ/", partOfSpeech: "n.", grammaticalCase: "主宾同形", definition: "犁宿星系；犁辕座", example: "Gericod Alueté tias!<br>欢迎来到犁宿星系！"},
     {word: "Benfoure", phonetic: "/ˈbɛnfɔuɻ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "本弗尔；本弗尔王国", example: "Gericod Benfoure tias!<br>欢迎来到本弗尔王国！"},
-    {word: "Eirine", phonetic: "/ɛiˈɻʌinɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "我", example: "Eirine necore et. 我是人。"},
+    {word: "Eirine", phonetic: "/ɛiˈɻʌinɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "我", example: "Eirine necor et.<br>我是人。"},
+    {word: "neocre", phonetic: "/ni'kʰɔɻɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "人；人类", example: "Coyare necore<br>人类社会"},
     {word: "Jogul", phonetic: "/ˈdʒɔkul/", partOfSpeech: "n.", grammaticalCase: "主宾同形", definition: "（本弗尔神话&珊教神话中的）使者；卿", example: "Benfou Montoré Jogul<br>梧桐十二卿"},
     {word: "zalone", phonetic: "/zʌˈlɔnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Màf zalone musbla scola daze musblitam dolént zalonseol aposisint zalon moš rimar.<br>每一粒沙都囚禁着一个世界，而世界本身，不过是另一片更辽阔沙海中的一粒沙。"},
     {word: "zalon", phonetic: "/zʌˈlɔn/", partOfSpeech: "n.", grammaticalCase: "宾格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Dal Musblae zalon et.<br>世界是一粒沙子。"},
@@ -10,7 +12,10 @@ let dictionaryData = [
     {word: "fulane", phonetic: "/fɯˈlʌnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "猫", example: "Kleš fulane Eirinam et.<br>这是我的猫。"},
     // 动词
     {word: "afuce", phonetic: "/ʌˈfjɯkʰɛ/", partOfSpeech: "v.", tense: "一般现在时", voice: "主动", definition: "加；增加", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
+    // 形容词
+    {word: "necorelam", phonetic: "/nikʰɔˈɻɛlʌm/", partOfSpeech: "adj.", definition: "自然的；纯净的", example: "Dal vacur necorelam, āmtanob et.<br>自然的馈赠是无价的。"},
     // 数词
+    {word: "atmoš", phonetic: "/ˈʌt̚mɔʃ/", partOfSpeech: "num./n.", grammaticalCase: "基数词", definition: "num.零；n.空", example: "Atmoš Eletène<br>一切皆空。"},
     {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.一；一个；adj.一个的；单独的", example: "Moš necore cinisó tias.<br>我一个人来到城里面。"},
     {word: "nam", phonetic: "/nʌm/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.二；两；两个；adj.两个的；不专注的；有二心的", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
     // 示例英文单词（24小时内删除）
@@ -67,33 +72,5 @@ let dictionaryData = [
         partOfSpeech: "n.",
         definition: "忧郁，悲伤",
         example: "There's a sense of melancholy in his music."
-    },
-    {
-        word: "paradigm",
-        phonetic: "/ˈpærədaɪm/",
-        partOfSpeech: "n.",
-        definition: "范例，示例；模式",
-        example: "This study represents a paradigm shift in our understanding of the disease."
-    },
-    {
-        word: "esoteric",
-        phonetic: "/ˌesəˈterɪk/",
-        partOfSpeech: "adj.",
-        definition: "深奥的，难懂的；只有内行才懂的",
-        example: "He studies esoteric philosophical texts."
-    },
-    {
-        word: "pragmatic",
-        phonetic: "/præɡˈmætɪk/",
-        partOfSpeech: "adj.",
-        definition: "务实的，实用的",
-        example: "We need to take a pragmatic approach to solving this problem."
-    },
-    {
-        word: "ambiguous",
-        phonetic: "/æmˈbɪɡjuəs/",
-        partOfSpeech: "adj.",
-        definition: "含糊的，不明确的；有歧义的",
-        example: "His instructions were ambiguous and open to interpretation."
     },
 ];
