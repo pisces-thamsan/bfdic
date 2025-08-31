5cf25308db6e4fa859297f5ed5190a1bbda6b000
澶山稵杞 <163642689+pisces-thamsan@users.noreply.github.com>
2025-08-31 14:26:20 +0800

    增加了一个词（）

--- dictionary.js
+++ dictionary.js
@@ -16,7 +16,7 @@ let dictionaryData = [
     {word: "Eirine", phonetic: "/ɛiˈɻʌinɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "我", example: "Eirine necor et.<br>我是人。"},
       // Ff
     {word: "fulane", phonetic: "/fɯˈlʌnɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "猫", example: "Kleš fulane Eirinam et.<br>这是我的猫。"},
-      //Ll
+      // Ll
     {word: "lucias", phonetic: "/ˈlɯk̚ʃiʌs/", partOfSpeech: "n.", grammaticalCase: "主宾同形", definition: "娱乐；消遣；露西亚（人名）", example: "Aege ath lucias<br>当作娱乐一般"}, 
       // Nn
     {word: "necore", phonetic: "/ni'kʰɔɻɛ/", partOfSpeech: "n.", grammaticalCase: "主格", definition: "人；人类", example: "Coyare necore<br>人类社会"},
@@ -25,7 +25,7 @@ let dictionaryData = [
     {word: "zalon", phonetic: "/zʌˈlɔn/", partOfSpeech: "n.", grammaticalCase: "宾格", definition: "沙；沙子；沙粒；硅；硅元素", example: "Dal Musblae zalon et.<br>世界是一粒沙子。"},
     // 动词
     {word: "afuce", phonetic: "/ʌˈfjɯkʰɛ/", partOfSpeech: "v.", tense: "一般现在时", voice: "主动", definition: "加；增加", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
-    {word: "reisen", phonetic: "/ˈɻɜisɛn/", partOfSpeech: "v.", tense: "一般现在时", voice: "被动语态", definition: "控制；操纵（reis的被动形式）", example: "Dal line corisön kí atzēn reisen<br>心儿被她如冰雪般控制着融化"},
+    {word: "reisen", phonetic: "/ˈɻɜisɛn/", partOfSpeech: "v.", tense: "一般现在时", voice: "被动", definition: "控制；操纵（reis的被动形式）", example: "Dal line corisön kí atzēn reisen<br>心儿被她如冰雪般控制着融化"},
     // 形容词
     {word: "necorelam", phonetic: "/nikʰɔˈɻɛlʌm/", partOfSpeech: "adj.", definition: "自然的；纯净的", example: "Dal vacur necorelam, āmtanob et.<br>自然的馈赠是无价的。"},
     // 数词
@@ -33,6 +33,7 @@ let dictionaryData = [
     {word: "moš", phonetic: "/mɔʃ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.一；一个；adj.一个的；单独的", example: "Moš necore cinisó tias.<br>我一个人来到城里面。"},
     {word: "nam", phonetic: "/nʌm/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.二；两；两个；adj.两个的；不专注的；有二心的", example: "Moš qoak moš afuce á nam.<br>一加上一等于二。"},
     {word: "truate", phonetic: "/t͡ʃʰɯʌtɛ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.三；三个；adj.三个的；层叠的", example: "Moš qoak nam afuce á truate.<br>一加上二等于三。"},
-    {word: "dior", phonetic: "/djɔɻ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.四；四个；adj.四个的；周围的；周身的", example: "Moš qoak truate afuce á dior.<br>一加上三等于四。"}
+    {word: "dior", phonetic: "/djɔɻ/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.四；四个；adj.四个的；周围的；周身的", example: "Moš qoak truate afuce á dior.<br>一加上三等于四。"},
+    {word: "ben", phonetic: "/bɛn/", partOfSpeech: "num./adj.", grammaticalCase: "基数词", definition: "num.五；五个；adj.五个的；多少；几多；几许", example: "Nam qoak truate afuce á ben.<br>二加上三等于五。"}
     // 语法格后缀
 ];
